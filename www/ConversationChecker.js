bf70a98e381e4eb29ae5c5840591719b8646b8dd
overbyte <allandt@gmail.com>
2014-01-09 16:15:09 +0000

    added console log to test

--- www/ConversationChecker.js
+++ www/ConversationChecker.js
@@ -5,6 +5,7 @@ var ConversationChecker = function () {
 	console.log('Conversation Checker is here'); 
 
 	window.echo = function(str, callback) {
+		console.log('echoing: ' + str);
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
         }, "Echo", "echo", [str]);
